fcfc71219b58056b432f023b77ce92be46af6c0c
lnicosia <lnicosia@student.42.fr>
2022-05-31 09:53:15 +0200

    [ADD] Script tests return values.

--- test_script.sh
+++ test_script.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-OPT="$@"
+OPT=""
 
-echo "$file diff:"
 ./ft_nm $OPT > ft_nm.txt
+echo "$?" > ft_nm-ret.txt
 nm $OPT > nm.txt
+echo "$?" > nm-ret.txt
+echo "Output diff:"
 diff ft_nm.txt nm.txt
+echo "Return diff:"
+diff ft_nm-ret.txt nm-ret.txt
